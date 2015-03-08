ae967ca3a90d08118bb71168db321bf21f3641b7
Carlos Andreu <cnandreu@gmail.com>
2015-03-07 21:24:37 -0600

    msg

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -7,9 +7,8 @@ app.get('/', function (req, res) {
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Running on:', host, port);
+  console.log('Running on port:', port);
 
 });

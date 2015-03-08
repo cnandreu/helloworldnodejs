6050c8df6f5380ba4be41c2ee365b62b170fc381
Carlos Andreu <cnandreu@gmail.com>
2015-03-07 22:49:14 -0600

    update with es

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var app = express();
 
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'es:9200',
+  log: 'trace'
+});
+
+
 app.get('/', function (req, res) {
   res.send('Hello World From a Docker Container :)!');
+});
+
+app.get('/es', function (req, res) {
+
+  client.search({
+    q: '1972'
+  }).then(function (body) {
+
+    var hits = body.hits.hits;
+    res.json(body);
+
+  }, function (error) {
+    console.trace(error.message);
+    res.send(error.message);
+  });
+
 });
 
 var server = app.listen(3000, function () {

29acd103785c61c910c8cc05a7ea83fcb2336834
Carlos Andreu <cnandreu@gmail.com>
2015-03-07 21:35:44 -0600

    update

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World From a Docker Container!');
+  res.send('Hello World From a Docker Container :)!');
 });
 
 var server = app.listen(3000, function () {
